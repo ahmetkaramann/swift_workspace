cac28da03a9ac988364b42e893401a25fdc5e728
Ahmet Karaman <123123422+ahmetkaramann@users.noreply.github.com>
2024-08-06 16:11:52 +0300

    You want to share any photos in the app

---
+++ SharedPhotosApp/FeedTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedTableViewCell.swift
+//  SharedPhotosApp
+//
+//  Created by Ahmet Karaman on 5.08.2024.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SharedPhotosApp/PostClass.swift
@@ -0,0 +1,8 @@
+//
+//  PostClass.swift
+//  SharedPhotosApp
+//
+//  Created by Ahmet Karaman on 6.08.2024.
+//
+
+import Foundation
